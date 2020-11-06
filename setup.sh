1b3b35a1c168455fdb0874daa6b70a15cddf8258
k0mn3 <39636110+k0mn3@users.noreply.github.com>
2020-11-06 19:41:55 +0100

    Add files via upload
    New Update

--- setup.sh
+++ setup.sh
@@ -1,27 +1,36 @@
+#!/bin/bash
 echo "#################### THIS SETUP SAVED ON GOINFRE IN ORDER TO SAVE SPACE ###################"
-if [ ! -d $HOME/goinfre/.brew ]
+# Setup variables 
+BREW_HOME=$HOME/.brew
+BREW_GOINFRE=$HOME/goinfre/.brew
+DOCKER_HOME=$DOCKER_HOME
+DOCKER_GOINFRE=$HOME/goinfre/.docker
+MINIKUBE_HOME=$HOME/.minikube
+MINIKUBE_GOINFRE=$HOME/goinfre/.minikube
+if [ ! -d $BREW_GOINFRE ]
 then
-	echo "------- setup HomeBrew -------"
-	rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/goinfre/.brew && echo 'export PATH=$HOME/goinfre/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+	echo "⭐️ $(tput setaf 1)------- setup HomeBrew -------$(tput sgr 0) ⭐️"
+	rm -rf BREW_HOME && git clone --depth=1 https://github.com/Homebrew/brew $BREW_GOINFRE && echo 'export PATH=$HOME/goinfre/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
 fi
-echo "------- Setup and start docker --------"
-[ ! -d $HOME/goinfre/.docker ] && brew install docker docker-machine
-docker-machine create --driver virtualbox default
-docker-machine start
+[ ! -d $DOCKER_GOINFRE ] && echo "⭐️ $(tput setaf 1)------- Setup docker --------$(tput sgr 0) ⭐️" && brew install docker docker-machine
+echo  "💨 $(tput setaf 5)------- Running Docker VM --------$(tput sgr 0) 💨"
+echo -n "😄  " ;docker-machine create --driver virtualbox default
+echo -n "🐳  " ;docker-machine start
 docker-machine env default
 eval "$(docker-machine env default)"
-if [ ! -d $HOME/goinfre/.docker ]
+if [ ! -d $DOCKER_GOINFRE ]
 then
-	mv $HOME/.docker $HOME/goinfre/.docker
-	ln -s $HOME/goinfre/.docker $HOME/.docker
+	mv $DOCKER_HOME $DOCKER_GOINFRE
+	ln -s $DOCKER_GOINFRE $DOCKER_HOME
 fi
-echo "------- Setup and start k8s + minikube ---------"
-[ ! -d $HOME/goinfre/.minikube ] && brew install minikube
+
+[ ! -d $MINIKUBE_GOINFRE ] && echo "⭐️ $(tput setaf 1)------- Setup k8s + minikube ---------$(tput sgr 0) ⭐️" && brew install minikube
+echo "💨 $(tput setaf 5)------- Running Minikube VM --------$(tput sgr 0) 💨"
 minikube start
-if [ ! -d $HOME/goinfre/.minikube ]
+if [ ! -d $MINIKUBE_GOINFRE ]
 then
-	mv $HOME/.minikube $HOME/goinfre/
-	ln -s $HOME/goinfre/.minikube $HOME/.minikube
+	mv $MINIKUBE_HOME $HOME/goinfre/
+	ln -s $MINIKUBE_GOINFRE $MINIKUBE_HOME
 fi
 
 echo "[+] DONE "
